e7ebb49b2d81aade9a1d617448da2e67817e7df5
tato469 <tato469@gmail.com>
2015-04-14 13:27:01 +0200

    HttpURLConnection urlConnection
    added HttpURLConnection urlConnection

--- routes googleMaps v2/JSONParser.java
+++ routes googleMaps v2/JSONParser.java
@@ -4,15 +4,19 @@ package your-package;
  * @author zeeshan0026
  */
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
@@ -31,13 +35,11 @@ public class JSONParser {
 
         // Making HTTP request
         try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();           
+            URL urlnew = new URL(url);
+            HttpURLConnection urlConnection = (HttpURLConnection) urlnew.openConnection();
+            is = new BufferedInputStream(urlConnection.getInputStream());
+
 
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
